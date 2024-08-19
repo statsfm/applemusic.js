c3cf4377655ed4ffeef9f50f4ebe8f4c284b9550
Roman Gafurov <gafurov.roma89@gmail.com>
2024-08-19 04:52:29 +0400

    chore(client): moved config to interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export * from './AppleMusicAPI';
-export * from './clientConfiguration';
 
 export * from './appleMusicError';
 export * from './util/CalendarDate';
 
 // Interfaces
+export * from './interfaces/Config';
 export * from './interfaces/AppleMusic';
